25e87db5f9f7d218edf58f56a63a8cac4f2fa7de
Stepan <Stepworm@yandex.ru>
2022-11-29 11:38:42 +0300

    Update str.cpp

--- assembly-exam/2/str.cpp
+++ assembly-exam/2/str.cpp
@@ -11,52 +11,52 @@ int main()
         mov esi, 0
         encrypt :
             mov al, 44;
-            mov bl, to_encrypt[esi]
+            mov bl, txt[esi]
             cmp al, bl
             jne ElsePart1
-            mov to_encrypt[esi], 42
+            mov txt[esi], 42
             jmp ElsePart1
 
                 ElsePart1:
                     mov al, 46;
                     cmp al, bl
                     jne ElsePart2
-                    mov to_encrypt[esi], 42
+                    mov txt[esi], 42
                     jmp ElsePart2
 
                     ElsePart2:
                         mov al, 59;
                         cmp al, bl
                         jne ElsePart3
-                        mov to_encrypt[esi], 42
+                        mov txt[esi], 42
                         jmp ElsePart3
 
                         ElsePart3:
                             mov al, 45;
                             cmp al, bl
                             jne ElsePart4
-                            mov to_encrypt[esi], 42
+                            mov txt[esi], 42
                             jmp ElsePart4
 
                             ElsePart4:
                                 mov al, 33;
                                 cmp al, bl
                                 jne ElsePart5
-                                mov to_encrypt[esi], 42
+                                mov txt[esi], 42
                                 jmp ElsePart5
 
                                 ElsePart5:
                                     mov al, 33;
                                     cmp al, bl
                                     jne ElsePart6
-                                    mov to_encrypt[esi], 42
+                                    mov txt[esi], 42
                                     jmp ElsePart6
 
                                     ElsePart6:
                                         mov al, 63;
                                         cmp al, bl
                                         jne ElsePart7
-                                        mov to_encrypt[esi], 42
+                                        mov txt[esi], 42
                                         jmp ElsePart7
 
                                         ElsePart7:
@@ -66,7 +66,7 @@ int main()
                                             jmp encrypt
         exit_ :
     }
-    std::cout << to_encrypt << std::endl;
+    std::cout << txt << std::endl;
 
     return 0;
 }
